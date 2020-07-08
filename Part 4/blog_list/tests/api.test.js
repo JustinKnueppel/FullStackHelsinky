dcbd60f62b190daff49e85481272e2284b090a44
Justin <justinknueppel@gmail.com>
2020-07-08 17:12:25 -0400

    Move tests to describe and do not log in tests

--- Part 4/blog_list/tests/api.test.js
+++ Part 4/blog_list/tests/api.test.js
@@ -14,82 +14,86 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-test("Notes returned as JSON", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
-
-test("Notes returns correct number", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(initialBlogs.length);
-});
-
-test("Notes should have id named id", async () => {
-  const response = await api.get("/api/blogs");
-  const firstBlog = response.body[0];
-  expect(firstBlog.id).toBeDefined();
-});
-
-test("Can add blog", async () => {
-  const newBlog = new Blog({
-    title: "New blog",
-    author: "Big Author",
-    url: "newblog.com",
-    likes: 1,
+describe("Retrieving all notes", () => {
+  test("Notes returned as JSON", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
   });
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const blogsAfterPost = await blogsInDb();
-  expect(blogsAfterPost).toHaveLength(initialBlogs.length + 1);
-
-  const titles = blogsAfterPost.map((blog) => blog.title);
-  expect(titles).toContain("New blog");
-});
-
-test("Likes default to 0", async () => {
-  const newBlog = new Blog({
-    title: "New blog",
-    author: "Big Author",
-    url: "newblog.com",
+  test("Notes returns correct number", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(initialBlogs.length);
   });
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  const blogsAfterPost = await blogsInDb();
-
-  const found = blogsAfterPost.find((blog) => blog.title === "New blog");
-  expect(found.likes).toEqual(0);
+  test("Notes should have id named id", async () => {
+    const response = await api.get("/api/blogs");
+    const firstBlog = response.body[0];
+    expect(firstBlog.id).toBeDefined();
+  });
 });
 
-test("Title is required for new blog", async () => {
-  const newBlog = new Blog({
-    author: "Big Author",
-    url: "newblog.com",
-    likes: 1,
+describe("Adding a blog", () => {
+  test("Can add blog with all required elements", async () => {
+    const newBlog = new Blog({
+      title: "New blog",
+      author: "Big Author",
+      url: "newblog.com",
+      likes: 1,
+    });
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAfterPost = await blogsInDb();
+    expect(blogsAfterPost).toHaveLength(initialBlogs.length + 1);
+
+    const titles = blogsAfterPost.map((blog) => blog.title);
+    expect(titles).toContain("New blog");
   });
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
-});
+  test("Likes default to 0", async () => {
+    const newBlog = new Blog({
+      title: "New blog",
+      author: "Big Author",
+      url: "newblog.com",
+    });
 
-test("Url is required for new blog", async () => {
-  const newBlog = new Blog({
-    title: "New blog",
-    author: "Big Author",
-    likes: 1,
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAfterPost = await blogsInDb();
+
+    const found = blogsAfterPost.find((blog) => blog.title === "New blog");
+    expect(found.likes).toEqual(0);
   });
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
+  test("Title is required for new blog", async () => {
+    const newBlog = new Blog({
+      author: "Big Author",
+      url: "newblog.com",
+      likes: 1,
+    });
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+  });
+
+  test("Url is required for new blog", async () => {
+    const newBlog = new Blog({
+      title: "New blog",
+      author: "Big Author",
+      likes: 1,
+    });
+
+    await api.post("/api/blogs").send(newBlog).expect(400);
+  });
 });
 
 afterAll(() => mongoose.connection.close());
