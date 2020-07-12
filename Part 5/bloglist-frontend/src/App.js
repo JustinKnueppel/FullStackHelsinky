3b0d12f202e1b90dfcf5746172bee9a1c78b48fa
Justin <justinknueppel@gmail.com>
2020-07-12 14:51:04 -0400

    Only show blog form when user logged in

--- Part 5/bloglist-frontend/src/App.js
+++ Part 5/bloglist-frontend/src/App.js
@@ -1,41 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import LoginForm from './components/LoginForm'
-import BlogForm from './components/BlogForm'
-import { getAll, postBlog } from "./services/blogs"
+import React, { useState, useEffect } from "react";
+import Blogs from "./components/Blogs";
+import LoginForm from "./components/LoginForm";
+import BlogForm from "./components/BlogForm";
+import { getAll, postBlog } from "./services/blogs";
 
 const App = () => {
-  const [user, setUser] = useState(null)
-  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null);
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   const addBlog = async (blog) => {
     const { token } = user;
 
     try {
-      const returnedBlog = await postBlog(blog, token)
-      setBlogs(blogs.concat(returnedBlog))
+      const returnedBlog = await postBlog(blog, token);
+      setBlogs(blogs.concat(returnedBlog));
     } catch (exception) {
-      alert("Unable to add blog")
+      alert("Unable to add blog");
     }
-
-  }
+  };
 
   return (
     <div>
-      <LoginForm setUser={setUser} />
-      <BlogForm addBlog={addBlog} />
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {user === null ? (
+        <LoginForm setUser={setUser} />
+      ) : (
+        <BlogForm addBlog={addBlog} />
       )}
+      <Blogs blogs={blogs} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
