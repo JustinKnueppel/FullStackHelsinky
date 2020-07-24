6059e8d8e2e6e178eb0f305846460b83decaf1dc
Justin <justinknueppel@gmail.com>
2020-07-24 15:27:40 -0400

    Add additional details to blog

--- Part 5/bloglist-frontend/src/components/Blog.js
+++ Part 5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,22 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
 
-export default Blog
+  return (
+    <div>
+      {blog.title}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "hide" : "details"}
+      </button>
+      {showDetails && (
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes}</p>
+          <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
