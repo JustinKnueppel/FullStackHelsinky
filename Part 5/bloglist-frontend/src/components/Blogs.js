0865e8bec566282cbfcd3d231399b68f2fe248b6
Justin <justinknueppel@gmail.com>
2020-07-24 16:06:04 -0400

    Sort blogs by likes

--- Part 5/bloglist-frontend/src/components/Blogs.js
+++ Part 5/bloglist-frontend/src/components/Blogs.js
@@ -4,7 +4,7 @@ import Blog from "./Blog";
 const Blogs = ({ blogs, likeBlog }) => (
   <div className="blogs">
     <h2>blogs</h2>
-    {blogs.map((blog) => (
+    {[...blogs].sort((b1, b2) => b1.likes < b2.likes).map((blog) => (
       <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
     ))}
   </div>
