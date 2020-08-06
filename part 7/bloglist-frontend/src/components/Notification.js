27b7b0ee1fe5239eb00ef6412709b4cd80cf0cb0
Justin <justinknueppel@gmail.com>
2020-08-06 17:39:29 -0400

    clean repo

--- part 7/bloglist-frontend/src/components/Notification.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Notification = ({ type, text }) => <div className={type}>{text}</div>;
-
-export default Notification;
--- part 7/routed-anecdotes/src/components/Notification.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Notification = ({ notification }) => {
-  return <div className={notification.type}>{notification.text}</div>;
-};
-
-export default Notification;
