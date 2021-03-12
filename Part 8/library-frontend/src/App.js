4f1db59258e8f3ebeccf5843c5b29a3fc02294d4
Justin Knueppel <justinknueppel@gmail.com>
2021-03-11 21:53:21 -0500

    Update birth year

--- Part 8/library-frontend/src/App.js
+++ Part 8/library-frontend/src/App.js
@@ -16,20 +16,22 @@ const ALL_AUTHORS = gql`
 `;
 
 const ALL_BOOKS = gql`
-query {
-  allBooks {
-    title
-    author
-    published
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
   }
-}
-`
+`;
 
 const App = () => {
   const [page, setPage] = useState("authors");
-  const result = useQuery(ALL_AUTHORS);
+  const result = useQuery(ALL_AUTHORS, {
+    pollInterval: 2000,
+  });
   const bookResult = useQuery(ALL_BOOKS, {
-    pollInterval: 2000
+    pollInterval: 2000,
   });
 
   if (result.loading) {
@@ -46,7 +48,7 @@ const App = () => {
 
       <Authors show={page === "authors"} authors={result.data.allAuthors} />
 
-      <Books show={page === "books"} books={bookResult.data?.allBooks}/>
+      <Books show={page === "books"} books={bookResult.data?.allBooks} />
 
       <NewBook show={page === "add"} />
     </div>
--- Part 8/library-frontend/src/components/Authors.js
+++ Part 8/library-frontend/src/components/Authors.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation, gql } from "@apollo/client";
+
+const EDIT_BIRTH_YEAR = gql`
+  mutation editBirthYear($name: String!, $born: Int!) {
+    editAuthor(name: $name, setBornTo: $born) {
+      name
+      born
+    }
+  }
+`;
 
 const Authors = ({ show, authors }) => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+  const [editAuthor] = useMutation(EDIT_BIRTH_YEAR);
+
   if (!show) {
     return null;
   }
+
+  const submitBorn = () => {
+    editAuthor({ variables: { name, born: parseInt(born) } });
+
+    setName("");
+    setBorn("");
+  };
 
   return (
     <div>
@@ -24,6 +45,26 @@ const Authors = ({ show, authors }) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <div>
+        Name:{" "}
+        <input
+          type="text"
+          value={name}
+          onChange={({ target }) => setName(target.value)}
+        />
+      </div>
+      <div>
+        Born:{" "}
+        <input
+          type="text"
+          value={born}
+          onChange={({ target }) => setBorn(target.value)}
+        />
+      </div>
+      <button type="submit" onClick={submitBorn}>
+        Submit
+      </button>
     </div>
   );
 };
